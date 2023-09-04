cca05b918a6eeb6bba1b379a22cbf7e56113645c
seizethedaylyn <22000189@handong.ac.kr>
2023-09-05 01:23:15 +0900

    Update Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Student s1 = new Student("22000111", "김채린");
+        Student s1 = new Student("22000189", "김채린");
         System.out.println(s1.toString());
     }
 }
